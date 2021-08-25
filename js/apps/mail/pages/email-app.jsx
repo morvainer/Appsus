277c02b261bf592b1b4384115b859cdbb17ab8a0
Mor Vainer <moryvan1@gmail.com>
2021-08-25 14:34:38 +0300

    email files

--- js/apps/mail/pages/email-app.jsx
+++ js/apps/mail/pages/email-app.jsx
@@ -1,9 +1,28 @@
+import { EmailList } from "../js/apps/mail/cmps/email-list.jsx"
+
+
+
 export class EmailApp extends React.Component {
+
+
+  state = {
+      
+
+  }
+
+  getTextToShow = (text) => {
+      
+  }
+
+
+
+
   render() {
     return (
-      <div>
+      <div className="email-app">
         <h1>This is my email app</h1>
         <h2>asdfafd</h2>
+        <EmailList/>
       </div>
     );
   }
