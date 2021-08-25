d9652dc36d8c8b7bfb1a6f43ce2a144387116cb5
Mor Vainer <moryvan1@gmail.com>
2021-08-25 12:38:21 +0300

    accept h2

--- js/apps/keep/cmps/note-list.jsx
+++ js/apps/keep/cmps/note-list.jsx
@@ -2,11 +2,7 @@ export class NoteList extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <h2>GIT</h2>
-=======
         <h2> COMMIT</h2>
->>>>>>> b9189b98b42a0a5bfc4b8395203e1238302d9b8b
         <h1>NoteList</h1>
       </div>
     );
